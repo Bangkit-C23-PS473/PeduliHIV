231b0d5933daafc3f9c115e9b0882dfb9c0d0535
Daniel Yulius S <danielyuliussoetjipto@gmail.com>
2023-06-15 02:18:41 +0700

    add custom dialog

--- app/src/main/java/com/danielys/pedulihiv/ui/home/HomeFragment.kt
+++ app/src/main/java/com/danielys/pedulihiv/ui/home/HomeFragment.kt
@@ -1,13 +1,16 @@
 package com.danielys.pedulihiv.ui.home
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.danielys.pedulihiv.R
 import com.danielys.pedulihiv.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -28,12 +31,23 @@ private var _binding: FragmentHomeBinding? = null
     _binding = FragmentHomeBinding.inflate(inflater, container, false)
     val root: View = binding.root
 
-//    val textView: TextView = binding.tvWelcome
-//    homeViewModel.text.observe(viewLifecycleOwner) {
-//      textView.text = it
-//    }
     binding.btnKonsul.setOnClickListener {
         Toast.makeText(context, "coba pencet", Toast.LENGTH_SHORT).show()
+
+        val customDialog = context?.let { it1 -> Dialog(it1) }
+        customDialog?.setContentView(R.layout.dialog_consul)
+
+        customDialog?.show()
+
+        val weight = customDialog?.findViewById<TextView>(R.id.et_weight)
+        val height = customDialog?.findViewById<TextView>(R.id.et_height)
+        val age = customDialog?.findViewById<TextView>(R.id.et_age)
+        val button = customDialog?.findViewById<Button>(R.id.btn_start_konsul)
+
+        button?.setOnClickListener {
+
+            customDialog.dismiss()
+        }
     }
     return root
   }
